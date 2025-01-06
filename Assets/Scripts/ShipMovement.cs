1472f0cb0b1bc7a94dd484e21c89977ffa840d81
Fabio Vitalba <fabvit9@gmail.com>
2025-01-06 15:05:46 +0100

    Terrain Update #1 - Added another village, improved shipwreck and added checkpoints (#5)
    * Changed Texture tiling
    
    * Added ship stand
    
    * Added second village on different island
    
    * Add shipwreck
    
    * Added hill on an island
    
    * Improve Shipwrecks
    
    * Lower seafloor
    
    * Update README
    
    * Reset Ship position and rotation on long land contact
    
    * Improved position storage
    
    * WIP - Attempt to rotate the ship according to the checkpoint
    
    * Fix Checkpoint storage and application
    
    * Fix second checkpoint on island

--- Assets/Scripts/ShipMovement.cs
+++ Assets/Scripts/ShipMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,10 +18,24 @@ public class ShipMovement : MonoBehaviour
     public float rudderDelta = 2.0f;
     public float maxRudder = 6.0f;
 
+    [Header("Land Collision")]
+    public int maxLandContactMillis = 1500;
+    // This variable stores the last Stand Checkpoint in order to return to the Stand if the player ever touches land for too long.
+    public Vector3 lastStandPosition;
+    public Vector3 lastStandEulerAngles;
+    public Quaternion lastStandRotation;
+    public DateTime lastLandContact = new DateTime();
+    public double millisSinceContactStart = 0;
+
+    private Rigidbody shipRigidbody = null;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        shipRigidbody = GetComponent<Rigidbody>();
+        lastStandPosition = transform.position;
+        lastStandRotation = transform.rotation;
+        lastStandEulerAngles = transform.eulerAngles;
     }
 
     // Update is called once per frame
@@ -77,5 +92,100 @@ public class ShipMovement : MonoBehaviour
         rotation.y = heading;
         rotation.z = -rudder;
         transform.eulerAngles = rotation;
+    }
+
+    public void ResetAcceleration() {
+        speed = 0.0f;
+    }
+
+    public void ResetSteering() {
+        rudder = 0.0f;
+        heading = 0.0f;
+    }
+
+    public void SetLastStandPosition(Vector3 newPosition, Vector3 newAngles, Quaternion newRotation) {
+        newPosition.y = lastStandPosition.y; // We want to maintain the same water-level, independently of the checkpoint height.
+        lastStandPosition = newPosition;
+        lastStandEulerAngles = newAngles;
+        lastStandRotation = newRotation;
+    }
+
+    public void ResetToLastStandPosition() {
+        ResetAcceleration();
+        ResetSteering();
+        shipRigidbody.velocity = Vector3.zero;
+        shipRigidbody.angularVelocity = Vector3.zero;
+        transform.position = lastStandPosition;
+        transform.rotation = lastStandRotation;
+        transform.eulerAngles = lastStandEulerAngles;
+        heading = transform.eulerAngles.y;
+    }
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        // Debug-draw all contact points and normals
+        // foreach (ContactPoint contact in collisionInfo.contacts) {
+        //     Debug.DrawRay(contact.point, contact.normal, Color.white);
+        // }
+        if (collisionInfo.transform.CompareTag("Land")) {
+            lastLandContact = DateTime.Now;
+        }
+    }
+
+    void OnCollisionExit(Collision collisionInfo)
+    {
+        lastLandContact = new DateTime();
+        millisSinceContactStart = 0;
+    }
+
+    void OnCollisionStay(Collision collisionInfo)
+    {
+        // Debug-draw all contact points and normals
+        // foreach (ContactPoint contact in collisionInfo.contacts) {
+        //     Debug.DrawRay(contact.point, contact.normal, Color.white);
+        // }
+        if (collisionInfo.transform.CompareTag("Land")) {
+            TimeSpan millisPassedSinceContact = DateTime.Now - lastLandContact;
+            millisSinceContactStart = millisPassedSinceContact.TotalMilliseconds;
+            if (millisSinceContactStart >= maxLandContactMillis) {
+                ResetToLastStandPosition();
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("ShipStand"))
+        {
+            Debug.Log("Storing Checkpoint!");
+            Transform checkpoint = FindChildWithTag(other.transform,"ShipStandCheckpoint");
+            if (checkpoint != null) {
+                Vector3 newStandPosition = checkpoint.position;
+                Vector3 newStandEulerAngles = checkpoint.eulerAngles;
+                Quaternion newStandRotation = checkpoint.rotation;
+                SetLastStandPosition(newStandPosition, newStandEulerAngles, newStandRotation);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        // if (other.CompareTag("ShipStand"))
+        // {
+        //     Debug.Log("Player left the Ship Stand area!");
+        // }
+    }
+
+    private Transform FindChildWithTag(Transform parent, string tag)
+    {
+        foreach (Transform child in parent)
+        {
+            if (child.CompareTag(tag))
+            {
+                return child;
+            }
+        }
+
+        return null; // Return null if no child with the tag is found
     }
 }
